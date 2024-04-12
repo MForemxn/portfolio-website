8f24779d1e0b49eba79415ee2d0657d6ce371719
Mason Foreman <104833724+MForemxn@users.noreply.github.com>
2024-04-12 16:26:39 +1000

    changed iframe url

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://portfolio-inner-site-iota.vercel.app/';
+        iframe.src = 'https://innersite.masonforeman.me/';
         /**
          * Use dev server is query params are present
          *
